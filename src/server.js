251d0e5f45ed3e393aa75ef08ec7c6004f7b603e
sirlawglobal <akanjilawrence9999@gmail.com>
2024-07-03 07:48:15 +0100

    Updated with npm packages again

--- src/server.js
+++ src/server.js
@@ -4,37 +4,57 @@ const realip = require('req-real-ip');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
+const geoip = require('geoip-lite');
 
 // Use the cors middleware
 app.use(cors());
+app.use(express.json());
 app.set('trust proxy', true);
 
 // Middleware to detect real IP
-app.use((req, res, next) => {
-  req.realIp = realip.detect({ req: req, config: { cloudflare: false } });
-  next();
-});
+// app.use((req, res, next) => {
+//   req.realIp = realip.detect({ req: req, config: { cloudflare: false } });
+//   next();
+// });
 
 const OPENWEATHER_API_KEY = process.env.OPENWEATHER_API_KEY;
 const IPIFY_API_KEY = process.env.IPIFY_API_KEY;
 
 app.get('/api/hello', async (req, res) => {
   const visitorName = req.query.visitor_name || 'Visitor';
-  let clientIp = req.realIp;
+
+  // let clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  // clientIp = clientIp.split(',')[0].trim();
+
+  let clientIp = req.headers["cf-connecting-ip"] || req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.headers['x-real-ip'] || req.socket.remoteAddress
+
+
+  // let clientIp = req.realIp;
 
   // Fallback IP for local testing
-  if (!clientIp || clientIp === '::1' || clientIp === '127.0.0.1') {
-    clientIp = '8.8.8.8'; // Example IP address (Google Public DNS)
-  }
+
+  // if (!clientIp || clientIp === '::1' || clientIp === '127.0.0.1') {
+  //   clientIp = '8.8.8.8'; // Example IP address (Google Public DNS)
+  // }
+
 
   console.log('Client IP:', clientIp); // Log client IP address
+  
+
+  // http://ip-api.com/json/${ip}?fields=city
 
   try {
-    const locationResponse = await axios.get(`https://geo.ipify.org/api/v1?apiKey=${IPIFY_API_KEY}&ipAddress=${clientIp}`);
+    // const locationResponse = await axios.get(`https://geo.ipify.org/api/v1?apiKey=${IPIFY_API_KEY}&ipAddress=${clientIp}`);
+
+    const geo = geoip.lookup(clientIp);
+    console.log('GEo:', geo); // Log client IP address
     
-    console.log('Location Response:', locationResponse.data); // Log location response
+    let city;
+  if (geo) {
+    city = `${geo.city}`;
+}
     
-    const { city } = locationResponse.data.location;
+
 
     if (!city) {
       return res.status(400).json({ error: 'City not found for the given IP address' });
